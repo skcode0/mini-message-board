3b10c923bc2648f9bdfe146cc03e2a2212d67e5d
sk <sk.paradoxmaske@gmail.com>
2021-06-02 18:06:35 -0500

    create message board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const timeFormat = require("date-fns/formatDistanceToNow");
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Home", messages, timeFormat });
+});
+
+// add new message to messages array
+router.post('/new', function(req, res){
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date()
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
